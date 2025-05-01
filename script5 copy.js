31429d6690f500b5a7426bf5a9d6121407bb57f7
Gabriel <grabezanany@gmail.com>
2025-04-30 18:13:45 -0600

    Add files via upload

---
+++ script5 copy.js
@@ -0,0 +1,7 @@
+var header = document.createElement("h3");
+header.innerText = "gz client";
+document.body.appendChild(header);
+
+var button = document.createElement("button");
+button.textContent = "This is a button";
+document.body.appendChild(button);
